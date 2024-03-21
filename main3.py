bf592968be81347535e4a3c110b17364ac9a33e9
alisson <alisson.ruiz@uteca.edu.mx>
2024-03-21 08:32:03 -0600

    Cambios

--- main3.py
+++ main3.py
@@ -25,12 +25,47 @@ grafoTest.addArista('E', 'D', 8)
 
 origen = 'A'
 destino = 'E'
-path[origen] = {'-':0}
-llaves = grafoTest.aristas['A'].keys()
-print(llaves)
-for i in llaves:
-    path[i]={origen: grafoTest.aristas}
-   
-print(grafoTest.aristas[origen])
+path={}
+visitados=[]
 
+visitados.append( origen )
+path[origen] = {'-':0}
+
+llaves = grafoTest.aristas[origen].keys()
+print(llaves)
+
+for i in llaves:
+    path[i]={origen: grafoTest.aristas[origen][i]}
+
+print("primer iter:")
+print(visitados)
+print(path)
+
+verticeAct = 'B'
+visitados.append( verticeAct )
+llaves = grafoTest.aristas[verticeAct].keys()
+print(llaves)
+
+for i in llaves:
+    if i not in visitados:
+        if i not in path: path[i]={}
+        llave = list( path[verticeAct].keys() )
+        acumulado = path[verticeAct][ llave[0] ]
+
+        path[i].update({ verticeAct : grafoTest.aristas[verticeAct][i]+acumulado })
+
+        #reviso si hay mas de 2 llaves en una llave del path
+        if len(path[i]) == 2:
+            kiss = list(path[i].keys())
+            if kiss[0]>kiss[1]:
+                del( path[i][ kiss[0] ])
+            elif kiss[0]<kiss[1]:
+                del( path[i][ kiss[1] ])
+            pass
+
+    #fin if not in visitados
+# fin for i
+
+print(path)
+exit()
 
